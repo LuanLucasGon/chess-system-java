6fcc7ce6b10ce3a11ebdf29b78b0a9dba074e36b
LuanLucasGon <lucasluangon@gmail.com>
2024-12-06 07:58:20 -0300

    feat: start moves feature

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private Board board;
 
@@ -13,5 +13,23 @@ public class Piece {
 
     protected Board getBoard() {
         return board;
+    }
+
+    public abstract  boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position position){
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove(){
+        boolean[][] moveMat = possibleMoves();
+        for(int i = 0; i < moveMat.length; i++){
+            for(int j = 0; j < moveMat.length; j++){
+                if(moveMat[i][j]){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -13,4 +13,10 @@ public class King extends ChessPiece {
     public String toString(){
         return "K";
     }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] moveMat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return moveMat;
+    }
 }
