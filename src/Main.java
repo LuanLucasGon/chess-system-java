389184436555e376be4479698b8f2a5245acca76
LuanLucasGon <lucasluangon@gmail.com>
2024-12-17 11:33:13 -0300

    feat: implementing promotion special move

--- src/Main.java
+++ src/Main.java
@@ -37,6 +37,14 @@ public class Main {
                 if(capturedPiece != null){
                     capturedPieces.add(capturedPiece);
                 }
+                if (chessMatch.getPromoted() != null) {
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = scanner.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+                        System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+                        type = scanner.nextLine().toUpperCase();
+                    }
+                }
             }catch (ChessException | InputMismatchException e){
                 System.out.println(e.getMessage());
                 scanner.nextLine();
